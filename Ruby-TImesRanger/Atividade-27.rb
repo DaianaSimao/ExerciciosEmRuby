cad5769345a9bb17069d0b07c31ae2c9769cca6f
DianaSimao <daiana.xd11@hotmail.com>
2022-06-30 19:01:56 -0300

    Exercícios proposto e resolvidos em Ruby

---
+++ Ruby-TImesRanger/Atividade-27.rb
@@ -0,0 +1,12 @@
+# Descrição: Considerando a variável abaixo:
+# inicio = 32
+# Imprima todos os números desde a variável inicio até o número 51.
+# Nome: Daiana Simão
+# Data:30/06/22
+
+
+inicio = 32
+
+(inicio..51).each do |n|
+    puts n 
+end
