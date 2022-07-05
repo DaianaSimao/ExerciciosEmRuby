07f7f09d6ca5dac6b0f09ca0bfdee7b0cd4e297f
DianaSimao <daiana.xd11@hotmail.com>
2022-07-04 23:06:26 -0300

    Exercícios propostos e resolvidos em Ruby

---
+++ Ruby-Metodos/Atividade-40.rb
@@ -0,0 +1,22 @@
+# Descrição: Considerando o código abaixo:
+
+# def segundos(minutos)
+# end
+
+# puts segundos(10) # Imprime 600
+# puts segundos(75) # Imprime 4500
+
+# O método segundos recebe um valor em minutos como parametro e retorna esse valor em segundos. Escreva o método segundos de forma que ele retorne os valores corretamente, como representado acima
+# Nome: Daiana Simão
+# Data: 04/07/22 
+
+
+
+
+def segundos(minutos)
+    segundos = minutos *60
+    return segundos
+end
+puts segundos(10)
+puts segundos(75)
+    
